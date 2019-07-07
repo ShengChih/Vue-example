f18676e59ec63fe35679b52ea45bf4a4622cdd03
ShengChih <ms0529756@gmail.com>
2019-07-07 18:44:29 +0800

    Add props for setting summand.

--- counter/index.js
+++ counter/index.js
@@ -2,15 +2,21 @@ var Counter = Vue.component('Counter', {
     template: `
         <div>
             <div>Count: {{ this.count }} </div>
-            <button class="btn btn-primary" v-on:click="increment">Incremnt by 1</button>
+            <button class="btn btn-primary" v-on:click="increment">Incremnt by {{ this.addNum }}</button>
         </div>
     `,
+    props: {
+        addNum: {
+            type: Number,
+            default: 1
+        }
+    },
     data: function () {
         return { count: 0 };
     },
     methods: {
         increment: function () {
-            this.count++;
+            this.count += this.addNum;
         }
     }
 });
