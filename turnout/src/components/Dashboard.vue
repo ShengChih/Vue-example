84f425c966775b63a7203113e78edb05ddf3f43c
ShengChih <ms0529756@gmail.com>
2019-07-14 13:55:33 +0800

    Add event database into firebase.

--- turnout/src/components/Dashboard.vue
+++ turnout/src/components/Dashboard.vue
@@ -2,14 +2,16 @@
     <div class="">
         <h3>Events Dashboard</h3>
         <button class="btn btn-danger btn-sm signout-btn" v-on:click="signOut">Sign Out</button>
-        Dashboard Component
-
-        {{$store.state}}
+        <hr>
+        <AddEvent />
+        <hr>
+        {{$store.state.events}}
     </div>
 </template>
 
 <script>
-import { firebaseApp } from '../firebaseApp';
+import { firebaseApp, eventsRef } from '../firebaseApp';
+import AddEvent from './AddEvent.vue';
 
 export default {
     methods: {
@@ -17,6 +19,18 @@ export default {
             this.$store.dispatch('signOut');
             firebaseApp.auth().signOut();
         }
+    },
+    components: {
+        AddEvent
+    },
+    mounted() {
+        eventsRef.on('value', snap => {
+            let events = []
+            snap.forEach(event => {
+                events.push(event.val())
+            })
+            this.$store.dispatch('setEvents', events)
+        });
     }
 }
 </script>
