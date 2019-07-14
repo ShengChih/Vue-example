56a9e80ff97693a76609e83d192ec4569217c634
ShengChih <ms0529756@gmail.com>
2019-07-14 10:09:39 +0800

    Add signout button

--- turnout/src/components/Dashboard.vue
+++ turnout/src/components/Dashboard.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="">
+        <h3>Events Dashboard</h3>
+        <button class="btn btn-danger btn-sm signout-btn" v-on:click="signOut">Sign Out</button>
         Dashboard Component
 
         {{$store.state}}
@@ -7,7 +9,15 @@
 </template>
 
 <script>
+import { firebaseApp } from '../firebaseApp';
+
+
 export default {
-    
+    methods: {
+        signOut() {
+            this.$store.dispatch('signOut');
+            firebaseApp.auth().signOut();
+        }
+    }
 }
 </script>
